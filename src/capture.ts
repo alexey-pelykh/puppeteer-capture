cbf9747bfdd14e04d2eda7e700ce05ac2a5db905
Alexey Pelykh <alexey.pelykh@gmail.com>
2022-05-26 16:49:17 +0200

    wip

--- src/capture.ts
+++ src/capture.ts
@@ -1,10 +1,11 @@
-import puppeteer from 'puppeteer' from 'puppeteer'
+import puppeteer from 'puppeteer'
 import { PuppeteerCapture } from './PuppeteerCapture'
 import { PuppeteerCaptureOptions } from './PuppeteerCaptureOptions'
 import { PuppeteerCaptureViaHeadlessExperimental } from './PuppeteerCaptureViaHeadlessExperimental'
 
 export async function capture (page: puppeteer.Page, options?: PuppeteerCaptureOptions): Promise<PuppeteerCapture> {
   if (process.platform === 'darwin') {
+    throw new Error()
   }
 
   return new PuppeteerCaptureViaHeadlessExperimental(page, options)
