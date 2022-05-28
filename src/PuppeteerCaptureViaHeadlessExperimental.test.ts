7ddf8b42f9a22d75837283375c31f2b6214eef58
Alexey Pelykh <alexey.pelykh@gmail.com>
2022-05-28 07:03:40 +0200

    wip

--- src/PuppeteerCaptureViaHeadlessExperimental.test.ts
+++ src/PuppeteerCaptureViaHeadlessExperimental.test.ts
@@ -3,19 +3,21 @@ import { PassThrough } from 'stream'
 import { launch } from './launch'
 import { PuppeteerCaptureViaHeadlessExperimental } from './PuppeteerCaptureViaHeadlessExperimental'
 
+const PUPPETEER_LAUNCH_ARGS = process.getuid() !== 0
+  ? []
+  : [
+      '--no-sandbox' // NOTE: https://github.com/puppeteer/puppeteer/issues/3698
+    ]
+
 let browser: puppeteer.Browser
 afterEach(async () => {
-  if (browser) {
+  if (browser != null) {
     await browser.close()
   }
 })
 
 test('that capture fails if required args are missing', async () => {
-  browser = await puppeteer.launch({
-    args: [
-      '--no-sandbox', // NOTE: https://github.com/puppeteer/puppeteer/issues/3698
-    ],
-  })
+  browser = await puppeteer.launch({ args: PUPPETEER_LAUNCH_ARGS })
   const page = await browser.newPage()
   expect(() => new PuppeteerCaptureViaHeadlessExperimental(page)).toThrow()
 })
@@ -23,20 +25,16 @@ test('that capture fails if required args are missing', async () => {
 test('that capture does not fail if required args are present', async () => {
   browser = await puppeteer.launch({
     args: [
-      '--no-sandbox', // NOTE: https://github.com/puppeteer/puppeteer/issues/3698
-      ...PuppeteerCaptureViaHeadlessExperimental.REQUIRED_ARGS,
-    ],
+      ...PUPPETEER_LAUNCH_ARGS,
+      ...PuppeteerCaptureViaHeadlessExperimental.REQUIRED_ARGS
+    ]
   })
   const page = await browser.newPage()
-  new PuppeteerCaptureViaHeadlessExperimental(page)
+  new PuppeteerCaptureViaHeadlessExperimental(page) // eslint-disable-line no-new
 })
 
 test('that capture works in headless mode', async () => {
-  browser = await launch({
-    args: [
-      '--no-sandbox', // NOTE: https://github.com/puppeteer/puppeteer/issues/3698
-    ],
-  })
+  browser = await launch({ args: PUPPETEER_LAUNCH_ARGS })
   const page = await browser.newPage()
   const capture = new PuppeteerCaptureViaHeadlessExperimental(page)
   const stream = new PassThrough()
@@ -48,11 +46,7 @@ test('that capture works in headless mode', async () => {
 })
 
 // test('that capture works repeatedly in headless mode', async () => {
-//   browser = await launch({
-//     args: [
-//       '--no-sandbox', // NOTE: https://github.com/puppeteer/puppeteer/issues/3698
-//     ],
-//   })
+//   browser = await launch({ args: PUPPETEER_LAUNCH_ARGS })
 //   const page = await browser.newPage()
 //   const capture = new PuppeteerCaptureViaHeadlessExperimental(page)
 //   await page.goto('https://google.com')
@@ -71,11 +65,7 @@ test('that capture works in headless mode', async () => {
 // })
 
 test('that capture works with custom viewport size', async () => {
-  browser = await launch({
-    args: [
-      '--no-sandbox', // NOTE: https://github.com/puppeteer/puppeteer/issues/3698
-    ],
-  })
+  browser = await launch({ args: PUPPETEER_LAUNCH_ARGS })
   const page = await browser.newPage()
   const capture = new PuppeteerCaptureViaHeadlessExperimental(page)
   const stream = new PassThrough()
@@ -88,11 +78,7 @@ test('that capture works with custom viewport size', async () => {
 })
 
 test('that capture drops captured frames', async () => {
-  browser = await launch({
-    args: [
-      '--no-sandbox', // NOTE: https://github.com/puppeteer/puppeteer/issues/3698
-    ],
-  })
+  browser = await launch({ args: PUPPETEER_LAUNCH_ARGS })
   const page = await browser.newPage()
   const capture = new PuppeteerCaptureViaHeadlessExperimental(page)
   const stream = new PassThrough()
@@ -105,29 +91,21 @@ test('that capture drops captured frames', async () => {
 })
 
 test('that capture stops gracefully on FFMPEG error', async () => {
-  browser = await launch({
-    args: [
-      '--no-sandbox', // NOTE: https://github.com/puppeteer/puppeteer/issues/3698
-    ],
-  })
+  browser = await launch({ args: PUPPETEER_LAUNCH_ARGS })
   const page = await browser.newPage()
   const capture = new PuppeteerCaptureViaHeadlessExperimental(page)
   const stream = new PassThrough()
   const error = new Error('fake')
   await page.goto('https://google.com')
   await capture.start(stream)
-  capture['_ffmpegStream']!.emit('error', error)
+  capture['_ffmpegStream']!.emit('error', error) // eslint-disable-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/dot-notation
   await expect(async () => {
     await capture.stop()
   }).rejects.toThrow(error)
 })
 
 test('that capture stops gracefully on page close', async () => {
-  browser = await launch({
-    args: [
-      '--no-sandbox', // NOTE: https://github.com/puppeteer/puppeteer/issues/3698
-    ],
-  })
+  browser = await launch({ args: PUPPETEER_LAUNCH_ARGS })
   const page = await browser.newPage()
   const capture = new PuppeteerCaptureViaHeadlessExperimental(page)
   const stream = new PassThrough()
